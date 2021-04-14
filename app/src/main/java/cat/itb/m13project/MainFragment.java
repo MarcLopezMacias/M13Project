0dd10d930ed039de1bbf31b4db5acd9587bdef7f
marc.lopezma.7e3@itb.cat <marc.lopezma.7e3@itb.cat>
2021-04-14 18:40:38 +0200

    Adding first fragments

---
+++ app/src/main/java/cat/itb/m13project/MainFragment.java
@@ -0,0 +1,29 @@
+package cat.itb.m13project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return v;
+    }
+}
