8e82eabf65e9654ce7cff70611f1530b9357135e
marc.lopezma.7e3@itb.cat <marc.lopezma.7e3@itb.cat>
2021-05-03 17:15:28 +0200

    Revert "Last Push + Test (test)"
    This reverts commit 01ee546e53ccba026967e0bc834d4dc19c4d30af.

--- app/src/main/java/cat/itb/m13project/MainActivity.java
+++ app/src/main/java/cat/itb/m13project/MainActivity.java
@@ -1,24 +1,11 @@
 package cat.itb.m13project;
 
-import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
